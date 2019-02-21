e3e12e2ee662275e30b2020aca5410a25dc2856b
David Rose <doppler@gmail.com>
2019-02-21 14:01:34 -0500

    added spin and gh-pages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,19 @@ const App = () => {
     svgDimensionsFromWindowSize()
   );
 
+  const [spinAngle, setSpinAngle] = useState(0);
+
+  let timeout;
+  useEffect(() => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      requestAnimationFrame(() => setSpinAngle(spinAngle + 1));
+    }, 1);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [spinAngle]);
+
   useEffect(() => {
     window.addEventListener("resize", () => {
       setSvgDimensions(svgDimensionsFromWindowSize);
@@ -22,15 +35,23 @@ const App = () => {
   return (
     <div id="App">
       <svg {...{ width, height }}>
-        {Array.from({ length: 6 }).map((_, i) => {
-          let angle = 60 * (i + 1);
-          let radian = (angle * Math.PI) / 180;
-          let center = {
-            x: width / 2 + r * 3 * Math.cos(radian),
-            y: height / 2 + r * 3 * Math.sin(radian)
-          };
-          return <SixCircleGroup key={i} center={center} rotate={angle - 60} />;
-        })}
+        <g style={{ transform: `rotate(${-spinAngle}deg)` }}>
+          {Array.from({ length: 6 }).map((_, i) => {
+            let angle = 60 * (i + 1);
+            let radian = (angle * Math.PI) / 180;
+            let center = {
+              x: width / 2 + r * 3 * Math.cos(radian),
+              y: height / 2 + r * 3 * Math.sin(radian)
+            };
+            return (
+              <SixCircleGroup
+                key={i}
+                center={center}
+                rotate={-spinAngle + 60 * i}
+              />
+            );
+          })}
+        </g>
       </svg>
     </div>
   );
@@ -54,7 +75,8 @@ const SixCircleGroup = ({ center, rotate }) => {
             key={i}
             {...{ cx, cy, r }}
             style={{
-              fill: `hsla(${angle}, 100%, 50%, 0.5)`
+              fill: `hsla(${angle / 6}, 100%, 25%, 0.1)`,
+              stroke: `hsl(${angle / 6}, 100%, 50%)`
             }}
           />
         );
