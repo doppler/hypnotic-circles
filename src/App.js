7f1dba1464e374276028eba41cbf91f1df8be060
David Rose <doppler@gmail.com>
2019-02-21 10:22:24 -0500

    basic 6-petals with hues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const svgDimensionsFromWindowSize = () => ({
+  width: window.innerWidth,
+  height: window.innerHeight
+});
+
 const App = () => {
+  const [{ width, height }, setSvgDimensions] = useState(
+    svgDimensionsFromWindowSize()
+  );
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setSvgDimensions(svgDimensionsFromWindowSize);
+    });
+  }, []);
+
+  const r = 100;
   return (
     <div id="App">
-      <h1>circle-grid</h1>
+      <svg {...{ width, height }}>
+        <circle cx={width / 2} cy={height / 2} r={r} />
+        {Array.from({ length: 6 }).map((_, i) => {
+          let angle = 60 * (i + 1);
+          let radian = (angle * Math.PI) / 180;
+          let cx = width / 2 + r * Math.cos(radian);
+          let cy = height / 2 + r * Math.sin(radian);
+          return (
+            <circle
+              {...{ cx, cy, r }}
+              style={{ fill: `hsl(${angle}, 100%, 50%, 0.5)` }}
+            />
+          );
+        })}
+      </svg>
     </div>
   );
 };
